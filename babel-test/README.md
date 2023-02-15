dda2bf5d1c8723e2e04e57195be3a07cbb881c6d
linzheng <alidlg9045@126.com>
2023-02-15 16:18:22 +0800

    Vite 降级插件@vitejs/plugin-legacy

--- babel-test/README.md
+++ babel-test/README.md
@@ -3,6 +3,4 @@
 1. 编译时工具
    * @babel/preset-env: useBuiltIns: "usage" / "entry"
 2. 更优的 Polyfill 注入方案: transform-runtime
-3. Vite 语法降级与 Polyfill 注入
-   * 
 
---
+++ vite-plugin-legacy/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy({
+      // 设置目标浏览器，browserslist 配置语法
+      targets: ['ie >= 11'],
+    })
+  ],
+})
